e90599aed6e792c3ccb549c8335aa4782fe7385d
Yuya Hochi <yuya1charo@gmail.com>
2020-12-25 13:45:21 +0900

    create 2-1

---
+++ JavaStudy/chapter1/src/chapter1/Ex1.java
@@ -0,0 +1,11 @@
+package chapter1;
+
+public class Ex1 {
+	public Ex1(String empId) {
+		System.out.println(empId);
+	}
+
+	public static void main(String[] args) {
+		Ex1 instance1 = new Ex1("charo");
+	}
+}
